5a6e6651948afe32f766aaff8d2da897e8faad60
Axel González Rodríguez <ed.dante.axel@gmail.com>
2020-04-10 20:07:16 -0500

    Add files via upload
    First step impression on screen of hello world and also the use of characters and artimetic operators with character string operation.

---
+++ cadenas.py
@@ -0,0 +1,58 @@
+#Caracteres de escape alt 91 \n salto linea \t tab #
+
+#comillas simples#
+
+cads = 'texto entre comillas simples'
+#comillas dobles#
+
+cadd = "entre comillas dobles"
+
+#mostrar en pantalla las candenas#
+
+print (type(cads))#imprime el tipo de archivo#
+
+print (cads)
+
+print (cadd)
+
+#repeticion y concatenacion#
+cad3 = "cadena"*3 #repetir strings#
+#muestras en pantalla#
+print (cad3) 
+#ejemplo de concatenacion#
+print  (cads + cadd)
+#bariables boleanas para evalucacion de info #
+verdad=True
+
+mentira=False
+
+Oand= True and False #operador logico que evalue que ambos sean verdaderos# 
+
+pand2 = True and True #pruebas de funcion#
+
+pand3 = False and False 
+
+print (pand2)
+
+print (pand3)
+
+OOr = True or False #si cualquiera de los dos cumple con verdadero regresa true#
+
+Por1= True or False #cumple el primero es verdadero#
+Por2= False or False #ninguno cumple es falso#
+
+print (Por1)
+
+print (Por2)
+
+Pnot = not True #combierte falso en verdadero y viceversa#
+
+Pnot1 = not True
+
+Pnot2 = not False
+
+print (Pnot1)
+
+print (Pnot2)
+
+
---
+++ enterosyoperadoresaritmeticos.py
@@ -0,0 +1,8 @@
+#existen dos tipos de enteros int mas corto y log mas largo numeros mas grandes#
+e=5 #int#
+e1=5 #long se agrega #
+flot = 0.334 #indicar variable con dec#
+
+print (e)
+print (e1)
+print (flot)
