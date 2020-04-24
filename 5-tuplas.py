20feedfecb62a7182d06db41be9ba450356c88f7
Axel González Rodríguez <ed.dante.axel@gmail.com>
2020-04-24 17:47:19 -0500

    Add files via upload

---
+++ 5-tuplas.py
@@ -0,0 +1,20 @@
+
+#en Python, una tupla es un conjunto ordenado e inmutable 
+#de  elementos del mismo o diferente tipo. Las tuplas se 
+#representan escribiendo los elementos entre paréntesis y
+#separados por comas.La diferencia con la lista es que los elementos de
+#la tupla son fijos y tampoco se puede modificar un solo elemento
+
+
+Tupla= 1, 2, 3
+
+print("\n")
+print(Tupla)
+
+#unicamente se puede modificar la tupla de manera completa
+
+Tupla= "hola", 55, 26
+
+print("\n")
+
+print(Tupla)
