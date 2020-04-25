272402d128529d7ee96ba27d07d591766e36201b
Axel González Rodríguez <ed.dante.axel@gmail.com>
2020-04-25 16:22:14 -0500

    Add files via upload

---
+++ 7-operadores.py
@@ -0,0 +1,57 @@
+#permiten comparar valiables, las comparaciones regresan resultados de true o false.
+
+a=1	
+b=2
+c=3
+# Primer operador evalua si los valores son iguales y soilo iguales
+
+ejemplo1= a==b    #<----- El primer ejemplo devuelve un false puesto que a no es igual a b
+
+print (ejemplo1)  
+print("\n")
+
+ejemplo2 = a == 1 #<----- El segundo ejemplo devuelve un false puesto que es igual el operador
+
+print (ejemplo2)
+print("\n")
+
+# Segundo operador permite evaluar si los valores son distintos
+
+ejemplo3 = a != b #<------   si estos son distintos nos estariaregresando valor true
+
+print(ejemplo3) 
+print("\n") 
+
+ejemplo4 = c != 3 # <-----   si los valores son iguales te entrega un valor false
+
+print(ejemplo4) 
+print("\n") 
+
+
+# El tercer grupo de operadores se conoce tien e los siguientes ejemplos
+# < El operador < evalua si el valor del lado izquierdo es menor que el valor del lado derecho
+# El operador > evalua si el valor del lado izquierdo es mayor que el valor del lado derecho.
+# El operador <= evalua si el valor del lado izquierdo es menor o igual que el valor del lado derecho
+# El operador >= evalua si el valor del lado izquierdo es mayor o igual que el valor del lado derecho.
+
+ejemplo5 = a < b  #<----  este ejemplo nos regresara un valor true puesto que 1 es menor que b
+
+print(ejemplo5)
+print("\n")
+
+ejemplo6 = a < 0 # <----  este ejemplo nos regresa un valor de false puesto que a es mayor
+print(ejemplo6)
+print("\n")
+#------------------------------------------------------------------------------------------
+ejemplo7 = a > b # <----  este ejemplo nos regresa un valor de false puesto que a es menor 
+print(ejemplo7)
+print("\n")
+
+ejemplo8 = c > b # <-----  este ejemplo no regresael valor true puesto que c es mayor
+print(ejemplo8)
+print("\n")
+ 
+ # el resto de los operadores mayoy e igual y menor e igual funciionan muy parecido solo que incluye
+ # le propiedad de cerradura de los numeros.
+
+
