c21a6132134fec50630407f2c1e81f55b8aecf7d
Axel González Rodríguez <ed.dante.axel@gmail.com>
2020-05-05 15:43:21 -0500

    Add files via upload

---
+++ 12-herencia.py
@@ -0,0 +1,44 @@
+
+#La herencia en Python nos permite a los programadores crear una clase general primero y luego más 
+#tarde crear clases más especializadas que re utilicen código de la clase general. 
+#La herencia también nos permite escribir un código más limpio y legible.
+
+class Clase:      
+	def __init__(self, tiempo): 
+		
+		print("\n")
+
+		self.tiempo = tiempo  
+
+		
+
+	def dialogo(self, mensaje):
+		mensaje = mensaje 
+		
+
+
+
+#----------
+
+class Ing(Clase):    #<----- primeramente la clase ingeniero hedera de la clase, Clase los metodos int y dialogo 
+# al ejecutar Ing automaticamente se ejecuta ing y opcionalmente podemos ejecutar dialogo
+	def programa(self, lenguaje):
+		print("voy a trabajar en", lenguaje)
+
+
+class Lic(Clase):
+	def demanda(self, caso):
+		print ("debo estudiar el caso ", caso)
+
+objeto  = Ing(25)
+
+objeto2 = Lic(22) 
+
+
+#-----------------------------------------
+
+print( objeto.programa('python'))  #<-----primeramente utilizamos el metodo demanda de la clase Ing
+
+print ("mi edad es", objeto.tiempo) # <---- de aqui estamos hederando el metodo init de Clase y usandolo en Ing
+
+objeto2.demanda('de las drogas')
