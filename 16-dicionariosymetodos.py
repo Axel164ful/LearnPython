bd92e45ef6c35d21c708e95e2c9c7dd8922a1917
Axel González Rodríguez <ed.dante.axel@gmail.com>
2020-05-09 19:16:02 -0500

    Add files via upload

---
+++ 16-dicionariosymetodos.py
@@ -0,0 +1,24 @@
+
+
+
+diccionario = { 
+	'mascotas' : ['perro', 'gato', "pez"], 
+	
+	2 : "edad",
+
+	'hola' : "mundo" 
+}
+
+diccionario.has_key("hola")#<--- este metodo se encarga de buscar una clave dentro del diccm regresatrue o false
+
+diccionario.items()#<--- regresa una lista de tuplas con clave y valor.
+
+diccionario.keys()#<--- regresa unicamente claves.
+
+diccionario.values()#<--- regresaunicamente los valores del diccionario.
+
+diccionario.pop( )#<----- extrae valor de de clavey lo borra
+
+diccionario.clear()#<--- borra todo el contenido del diccionario
+
+diccionario.copy()#<--- crea una copia completa del diccionario.
