4d56dff3c132c3438e0f010bc11283cd58143eb7
Axel González Rodríguez <ed.dante.axel@gmail.com>
2020-07-08 19:51:37 -0500

    Add files via upload

---
+++ 29-paquetes.py
@@ -0,0 +1,14 @@
+#permiten ordenar modulos
+
+#para que una carpeta pueda reconocer un paquete la carpeta debe contener el archivo __init__.py
+
+
+# para importar un modulo se sigue la sig estructura.
+
+import nombrecarpeta.nombremodulo
+
+
+nombrecarpeta.nombremodulo.ejemplo() #la itilizacion es igual a la de modulos.py
+
+
+from nombrecarpeta import nombremodulo
