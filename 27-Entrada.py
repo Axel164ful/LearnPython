097edd5768f0a31f2ea1cdad663a8c28c0622d09
Axel González Rodríguez <ed.dante.axel@gmail.com>
2020-07-08 19:51:15 -0500

    Add files via upload

---
+++ 27-Entrada.py
@@ -0,0 +1,13 @@
+#para guardar texto se utliza el comando raw_input
+try:
+	valor = raw_input("dame numero") #< --- permite la entrada de informacion en forma de cadenas de str
+	valor = int(valor)
+except:
+	print ("error")
+else:
+	print (valor)
+	
+# salida estandar
+#opciones de salida con print.
+
+
